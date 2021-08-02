dc8e47743a05fde60affb924c0f956847f45f556
Szekely Attila <attila.szekely@higg.com>
2021-08-02 13:53:16 +0300

    volume needs debugging

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{" 💻 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{" 🔊  ","echo $(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')", 2,	   10},
+
+	{" 🕑 ", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
